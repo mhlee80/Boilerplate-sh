6bc3fb4a3556d034f6f1d16a37c53a06173eee97
mhlee80 <myeonghyeon.lee@gmail.com>
2020-03-14 13:52:36 +0900

    update gen-sls-api. add post, get, delete, list

--- gen-sls-api.sh
+++ gen-sls-api.sh
@@ -24,17 +24,57 @@ if [ -z $basePath ] ; then
   echo $usage; exit 1;
 fi
 
-apiGroupId=$apiGroup-$apiVersion-$basePath
-functionsFilename=serverless-$apiGroupId.yaml
+functionsFilename=serverless-$apiGroup-$apiVersion-$basePath.yaml
 
 echo """\
+post:
+  name: \${self:custom.apiName}-post
+  handler: src/functions/api-handlers/\${self:custom.apiGroup}-\${self:custom.apiVersion}/\${self:custom.basePath}/post.handler
+  events:
+    - http:
+        path: /
+        method: post
+        integration: lambda-proxy
+        cors: true
+
 get:
   name: \${self:custom.apiName}-get
-  handler: src/functions/api-handlers/\${self:custom.apiGroupId}/\${self:custom.basePath}/get.handler
+  handler: src/functions/api-handlers/\${self:custom.apiGroup}-\${self:custom.apiVersion}/\${self:custom.basePath}/get.handler
   events:
     - http:
         path: /
         method: get
         integration: lambda-proxy
         cors: true
+
+delete:
+  name: \${self:custom.apiName}-delete
+  handler: src/functions/api-handlers/\${self:custom.apiGrou}-\${self:custom.apiVersion}/\${self:custom.basePath}/delete.handler
+  events:
+    - http:
+        path: /{id}
+        method: delete
+        request:
+          parameters:
+            paths:
+              id: true
+        integration: lambda-proxy
+        cors: true
+
+list:
+  name: \${self:custom.apiName}-list
+  handler: src/functions/api-handlers/\${self:custom.apiGroup}-\${self:custom.apiVersion}/\${self:custom.basePath}/list.handler
+  events:
+    - http:
+        path: /
+        method: get
+        request:
+          parameters:
+            querystrings:
+              url: true
+        integration: lambda-proxy
+        cors: true
 """ > $functionsFilename
+
+mkdir -p src/functions/api-handlers/$apiGroup-$apiVersion/$basePath
+
